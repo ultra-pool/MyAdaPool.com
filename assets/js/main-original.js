ea875ea95193721b622cbf8da7bd5427d237dfeb
Dao Lam <daolavi@gmail.com>
2019-03-22 22:16:41 +0700

    Update https for api request

--- assets/js/main-original.js
+++ assets/js/main-original.js
@@ -217,7 +217,7 @@
     $("#submitEmail").click(function () {
       var email = $('#email').val();
       if (email) {
-        $.get('http://45.119.81.115:35001/api/myadapool/subscribe?email=' + email, function (response) {
+        $.get('https://ez-exe.com:35002/api/myadapool/subscribe?email=' + email, function (response) {
         });
         toastr["success"]('Thank you for your subscription.');
       }
