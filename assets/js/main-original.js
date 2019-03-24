f777556ef62b80bf24677732eecfa52a38e71caa
Dao Lam <daolavi@gmail.com>
2019-03-24 18:45:56 +0700

    Update api url

--- assets/js/main-original.js
+++ assets/js/main-original.js
@@ -217,7 +217,7 @@
     $("#submitEmail").click(function () {
       var email = $('#email').val();
       if (email) {
-        $.get('https://subscribe.myadapool.com/api/myadapool/subscribe?email=' + email, function (response) {
+        $.get('https://ez-exe.com:35002/api/MyAdaPool/subscribe?email=' + email, function (response) {
         });
         toastr["success"]('Thank you for your subscription.');
       }
